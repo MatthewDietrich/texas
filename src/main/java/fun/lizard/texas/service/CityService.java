06c77c1ed79175971522e2f080199ddcb12e5b3c
Matt Dietrich <mbd321123@gmail.com>
2025-05-19 09:37:52 -0500

    adjust city plotting

--- src/main/java/fun/lizard/texas/service/CityService.java
+++ src/main/java/fun/lizard/texas/service/CityService.java
@@ -102,7 +102,7 @@ public class CityService {
         int drawWidth = width - 2 * borderWidth;
         int drawHeight = width - 2 * borderWidth;
         int x = (int) ((longitude - minLon) / (maxLon - minLon) * drawWidth);
-        int y = (int) ((maxLat - latitude) / (maxLat - minLat) * drawHeight) + borderWidth / 2;
+        int y = (int) ((maxLat - latitude) / (maxLat - minLat) * drawHeight) + borderWidth;
         g2d.setColor(Color.decode("#268bd2"));
         g2d.fillOval(x - 5, y - 5, 20, 20);
 
